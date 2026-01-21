21c467277578474f315d25ac17cea0e32da268b9
ruspian <ruspianntb@gmail.com>
2026-01-21 16:41:45 +0800

    tampilan produk

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,4 +1,5 @@
 import KategoriFilter from "@/components/KategoriFilter";
+import { PlaceCard } from "@/components/ui/card-22";
 import { KategoriType } from "@/types/category";
 
 interface HomeProps {
@@ -7,8 +8,76 @@ interface HomeProps {
 
 export default async function HomePage({ searchParams }: HomeProps) {
   const params = await searchParams;
+
   // Ambil kategori dari URL
   const activeCategory = (params.kategori as KategoriType) || "Semua";
+
+  const demoPlaceData = [
+    {
+      images: [
+        "https://images.unsplash.com/photo-1589182373726-e4f658ab50f0?q=80&w=2940&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1596622247990-84877175438a?q=80&w=2864&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1543332164-6e82f355badc?q=80&w=2940&auto=format&fit=crop",
+      ],
+      tags: ["Adventure", "Ancient Monuments"],
+      rating: 4.8,
+      title: "Petra, Jordan",
+      dateRange: "May 1 - 6",
+      hostType: "Business host",
+      isTopRated: true,
+      description:
+        "A lost city carved in rose-colored stone, hidden in majestic desert canyons.",
+      pricePerNight: 139,
+    },
+    {
+      images: [
+        "https://images.unsplash.com/photo-1589182373726-e4f658ab50f0?q=80&w=2940&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1596622247990-84877175438a?q=80&w=2864&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1543332164-6e82f355badc?q=80&w=2940&auto=format&fit=crop",
+      ],
+      tags: ["Adventure", "Ancient Monuments"],
+      rating: 4.8,
+      title: "Petra, Jordan",
+      dateRange: "May 1 - 6",
+      hostType: "Business host",
+      isTopRated: true,
+      description:
+        "A lost city carved in rose-colored stone, hidden in majestic desert canyons.",
+      pricePerNight: 139,
+    },
+    {
+      images: [
+        "https://images.unsplash.com/photo-1589182373726-e4f658ab50f0?q=80&w=2940&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1596622247990-84877175438a?q=80&w=2864&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1543332164-6e82f355badc?q=80&w=2940&auto=format&fit=crop",
+      ],
+      tags: ["Adventure", "Ancient Monuments"],
+      rating: 4.8,
+      title: "Petra, Jordan",
+      dateRange: "May 1 - 6",
+      hostType: "Business host",
+      isTopRated: true,
+      description:
+        "A lost city carved in rose-colored stone, hidden in majestic desert canyons.",
+      pricePerNight: 139,
+    },
+    {
+      images: [
+        "https://images.unsplash.com/photo-1589182373726-e4f658ab50f0?q=80&w=2940&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1596622247990-84877175438a?q=80&w=2864&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1543332164-6e82f355badc?q=80&w=2940&auto=format&fit=crop",
+      ],
+      tags: ["Adventure", "Ancient Monuments"],
+      rating: 4.8,
+      title: "Petra, Jordan",
+      dateRange: "May 1 - 6",
+      hostType: "Business host",
+      isTopRated: true,
+      description:
+        "A lost city carved in rose-colored stone, hidden in majestic desert canyons.",
+      pricePerNight: 139,
+    },
+  ];
 
   return (
     <main className="flex flex-col items-center justify-center max-w-7xl mx-auto px-4 w-full">
@@ -19,10 +88,26 @@ export default async function HomePage({ searchParams }: HomeProps) {
         <KategoriFilter />
       </section>
 
-      <section className="mt-4">
+      <section className="mt-2 w-full">
         <p className="text-gray-500">
           Menampilkan produk untuk: <strong>{activeCategory}</strong>
         </p>
+        <div className="grid grid-cols-6 space-x-4">
+          {demoPlaceData.map((place, index) => (
+            <PlaceCard
+              key={index}
+              images={place.images}
+              tags={place.tags}
+              rating={place.rating}
+              title={place.title}
+              dateRange={place.dateRange}
+              hostType={place.hostType}
+              isTopRated={place.isTopRated}
+              description={place.description}
+              pricePerNight={place.pricePerNight}
+            />
+          ))}
+        </div>
       </section>
     </main>
   );
--- components/KategoriFilter.tsx
+++ components/KategoriFilter.tsx
@@ -22,12 +22,12 @@ const KategoriFilter = () => {
     <div className="grid grid-cols-3 md:flex items-center  gap-3 overflow-x-auto py-6 no-scrollbar">
       {KategoriIcon.map((category) => {
         const IconComponent = category.icon; // ambil iconnya
-        const isActive = activeCategory === category.name;
+        const isActive = activeCategory === category.name; // kategori yang aktif
         return (
           <button
             key={category.name}
             onClick={() => handleFilter(category.name)}
-            className={`px-6 py-2 rounded-full whitespace-nowrap text-xs md:text-sm  font-medium transition-all flex gap-2 items-center justify-center 
+            className={`mx-1 px-4 py-2 rounded-full whitespace-nowrap text-xs md:text-sm  font-medium transition-all flex gap-2 items-center justify-center hover:scale-105 duration-100 cursor-pointer
             ${
               isActive
                 ? "bg-orange-500 text-white shadow-md"
