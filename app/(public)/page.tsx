976fdd21657dfb7c384ab45f83ab32989157336f
ruspian <ruspianntb@gmail.com>
2026-01-21 15:26:01 +0800

    uhab kategori ke server side

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,29 @@
-import KategoryClient from "@/components/KategoriClient";
+import KategoriFilter from "@/components/KategoriFilter";
+import { KategoriType } from "@/types/category";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: { kategori?: string };
+}
+
+export default async function HomePage({ searchParams }: HomeProps) {
+  const params = await searchParams;
+  // Ambil kategori dari URL
+  const activeCategory = (params.kategori as KategoriType) || "Semua";
+
   return (
-    <>
-      <KategoryClient />
-    </>
+    <main className="flex flex-col items-center justify-center max-w-7xl mx-auto px-4 w-full">
+      <section>
+        <h2 className="text-xl font-bold mt-8 text-gray-800 dark:text-orange-500">
+          Telusuri Kategori
+        </h2>
+        <KategoriFilter />
+      </section>
+
+      <section className="mt-4">
+        <p className="text-gray-500">
+          Menampilkan produk untuk: <strong>{activeCategory}</strong>
+        </p>
+      </section>
+    </main>
   );
 }
--- components/KategoriFilter.tsx
+++ components/KategoriFilter.tsx
@@ -1,35 +1,43 @@
 "use client";
 
 import { KategoriIcon, KategoriType } from "@/types/category";
-import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
-interface CategoryFilterProps {
-  selectedCategory: KategoriType;
-  onSelectCategory: (category: KategoriType) => void;
-}
+const KategoriFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-const KategoriFilter: React.FC<CategoryFilterProps> = ({
-  selectedCategory,
-  onSelectCategory,
-}) => {
+  const activeCategory =
+    (searchParams.get("kategori") as KategoriType) || "Semua";
+
+  const handleFilter = (name: KategoriType) => {
+    if (name === "Semua") {
+      router.push("/");
+    } else {
+      router.push(`?kategori=${name}`);
+    }
+  };
+
   return (
-    <div className="flex items-center gap-3 overflow-x-auto py-6 no-scrollbar">
+    <div className="grid grid-cols-3 md:flex items-center  gap-3 overflow-x-auto py-6 no-scrollbar">
       {KategoriIcon.map((category) => {
         const IconComponent = category.icon; // ambil iconnya
+        const isActive = activeCategory === category.name;
         return (
           <button
             key={category.name}
-            onClick={() => onSelectCategory(category.name)}
-            className={`px-6 py-2 rounded-full whitespace-nowrap text-sm font-medium transition-all flex gap-2 items-center justify-center
+            onClick={() => handleFilter(category.name)}
+            className={`px-6 py-2 rounded-full whitespace-nowrap text-xs md:text-sm  font-medium transition-all flex gap-2 items-center justify-center 
             ${
-              selectedCategory === category.name
+              isActive
                 ? "bg-orange-500 text-white shadow-md"
                 : "bg-gray-100 text-gray-600 hover:bg-gray-200"
             }`}
           >
             <IconComponent
               size={18}
-              strokeWidth={selectedCategory === category.name ? 2.5 : 2}
+              strokeWidth={isActive ? 2.5 : 2}
+              className="hidden md:block"
             />
             <span>{category.name}</span>
           </button>
